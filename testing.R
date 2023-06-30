cf4dd4ec0fd3f382170d124e2f9a65d1658f4a18
Adam Kapelner <kapelner@gmail.com>
2023-06-29 22:50:19 -0400

    renamed function per Dirk's comment

--- testing.R
+++ testing.R
@@ -1,10 +1,11 @@
-pacman::p_load(bare, microbenchmark)
+library(microbenchmark)
+library(bare)
 
 p = 100
 M = matrix(rnorm(p^2), nrow = p)
 
 Rcpp::cppFunction(depends = "RcppEigen", '					
-	double eigen_det_cpp(const Eigen::Map<Eigen::MatrixXd> X, int n_cores) {
+	double eigen_det_cpp_on_the_fly(const Eigen::Map<Eigen::MatrixXd> X, int n_cores) {
 		Eigen::setNbThreads(n_cores);
 		return X.determinant();
 	}
@@ -12,6 +13,6 @@ Rcpp::cppFunction(depends = "RcppEigen", '
 
 microbenchmark(
   within_package =  eigen_det(M, 1),
-  on_the_fly =      eigen_det_cpp(M, 1),
+  on_the_fly =      eigen_det_cpp_on_the_fly(M, 1),
   times = 10
 )
